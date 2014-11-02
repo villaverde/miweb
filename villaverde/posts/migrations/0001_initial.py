483cc420de1fcbfeab6364950a9166638b932b37
Manuel Villaverde <vialmajo@gmail.com>
2014-11-02 22:02:17 +0100

    Cramos el modelo de la app con post, category y image

---
+++ villaverde/posts/migrations/0001_initial.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('slug', models.SlugField(max_length=110)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to=b'PostsImages')),
+                ('upload_date', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=210)),
+                ('body', models.TextField()),
+                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('update_date', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(to='posts.Category')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='image',
+            name='post',
+            field=models.ForeignKey(to='posts.Post'),
+            preserve_default=True,
+        ),
+    ]
--- villaverde/posts/models.py
+++ villaverde/posts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=110)
+
+	def __str__(self):
+		return self.name
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=200) #Titulo de los post
+	slug = models.SlugField(max_length=210) #Slug del post
+	body = models.TextField() #Contenido del post
+	creation_date = models.DateTimeField(auto_now_add=True) #Fecha de creacion post
+	update_date = models.DateTimeField(auto_now=True) #Fecha edicion post
+	user = models.ForeignKey(User) #Usuario al que pertenece el post
+	category = models.ForeignKey(Category) #Categoria a la que pertenece el post
+
+	def __str__(self):
+		return self.title
+
+
+class Image(models.Model):
+	post = models.ForeignKey(Post)
+	image = models.ImageField(upload_to='PostsImages')
+	upload_date=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.image
+
+
