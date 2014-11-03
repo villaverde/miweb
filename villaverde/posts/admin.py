cdec93a613143d546e02a3f80a15c5e08f4d20c6
Manuel Villaverde <vialmajo@gmail.com>
2014-11-03 13:09:01 +0100

    Registramos el admin para category, image y post

--- villaverde/posts/admin.py
+++ villaverde/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Image
+
+class ImageInline(admin.StackedInline):
+	model = Image
+	extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'creation_date', 'category', 'user')
+	inlines = [ImageInline,]
+
+admin.site.register(Category)
+admin.site.register(Image)
